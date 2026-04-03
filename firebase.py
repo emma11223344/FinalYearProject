9d6eb20325e5ee48587626d361c1ea9f94d45788
Emma <e.fahy26@universityofgalway.ie>
2026-04-03 20:18:19 +0100

    Reapply "trying to fix firebase config to fix render"
    This reverts commit 1aa8f710ce90ce4fa7bdfb1434a3df7435797d0b.

--- firebase.py
+++
@@ -1,8 +0,0 @@
-import firebase_admin
-from firebase_admin import credentials, firestore
-
-cred = credentials.Certificate("fyp-2026-47ce8-firebase-adminsdk-fbsvc-8a46a9f49d.json")
-
-firebase_admin.initialize_app(cred)
-
-db = firestore.client()
